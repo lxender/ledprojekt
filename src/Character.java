f44d0f5fb2bde29332ba7d3ff0785abc32584cb7
lxender <hammes_alexander@yahoo.com>
2018-12-12 21:38:19 +0100

    Man kann nun nicht mehr gedrückt halten zum Laufen

--- src/Character.java
+++ src/Character.java
@@ -34,7 +34,7 @@ public class Character {
 
     public void update(KeyEvent event) {
         if(event != null) {
-            if(event.getID() == java.awt.event.KeyEvent.KEY_RELEASED) {
+            if(event.getID() == KeyEvent.KEY_RELEASED) {
                 switch (event.getKeyCode()) {
                     case KeyEvent.VK_UP:
                         this.yPos -= 1;
