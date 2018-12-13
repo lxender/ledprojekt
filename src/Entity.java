d34c505bfbf6f0ed6dc83cf81524b2d05c07fb18
lxender <hammes_alexander@yahoo.com>
2018-12-13 21:55:08 +0100

    Möglichkeit zum Flippen der Modelle hinzugefügt. Versucht einen Ordner für traits-related Code zu erstellen, aber hab gefailed. Hoffe, hiermit wird kein Ordner hochgeladen

---
+++ src/Entity.java
@@ -0,0 +1,13 @@
+import ledControl.BoardController;
+
+import java.awt.event.KeyEvent;
+
+public abstract class Entity {
+    public abstract void setX(int x);
+    public abstract int getX();
+    public abstract void setY(int x);
+    public abstract int getY();
+    public abstract void addTrait(Trait trait);
+    public abstract void update(KeyEvent event);
+    public abstract void draw(BoardController controller);
+}
---
+++ src/Model.java
@@ -0,0 +1,113 @@
+import ledControl.BoardController;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Model {
+    private int[][][] model;
+
+    public Model(int[][][] array) {
+        this.model = array;
+    }
+
+    public Model(int[][] array, int width, int height) {
+        this.model = new int[width][height][4];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                this.model[i][j] = array[i];
+            }
+        }
+    }
+
+    public Model(String path, int[] attachmentLocation) {
+        File file;
+        BufferedImage img;
+        try {
+            file = new File(path);
+            img = ImageIO.read(file);
+
+            int width = img.getWidth();
+            int height = img.getHeight();
+
+            int[][][] result = new int[height][width][];
+            for (int row = 0; row < height; row++) {
+                for (int col = 0; col < width; col++) {
+                    int p = img.getRGB(col, row);
+                    Color c = new Color(p, true);
+                    int[] rgba;
+                    if (c.getAlpha() == 255) {
+                        rgba = new int[]{c.getRed(), c.getGreen(), c.getBlue(), (c.getAlpha() == 255) ? 1 : 0};
+                    } else {
+                        rgba = new int[]{0, 0, 0, 0};
+                    }
+                    result[row][col] = rgba;
+//                    System.out.print(Arrays.toString(rgba));
+                }
+//                System.out.print("\n");
+            }
+            this.model = result;
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void appendAttachment(Model attachment, int x, int y) {
+        int[] attachmentLocation = {x, y};
+        int[][][] array = attachment.get2DArray();
+        for (int row = array.length - 1; row >= 0; row--) {
+            for (int col = 0; col < array[row].length; col++) {
+                this.model[attachmentLocation[1] - row][attachmentLocation[0]] = array[row][col];
+            }
+        }
+    }
+
+    public void flip() {
+        for(int i = 0; i < this.model.length; i++) {
+            int[][] flipped = new int[this.model[i].length][4];
+            for (int j = this.model[i].length - 1; j > 0; j--) {
+                flipped[j] = this.model[i][j];
+            }
+            this.model[i] = flipped;
+        }
+    }
+
+    public int[][][] get2DArray() {
+        return this.model;
+    }
+
+    public int[][] toSimpleArray() {
+        int[][] temp = new int[0][4];
+        for (int i = 0; i < this.model.length; i++) {
+            for (int j = 0; j < this.model[0].length; j++) {
+                temp = Arrays.copyOf(temp, temp.length + 1);
+                temp[temp.length - 1] = this.model[i][j];
+            }
+        }
+        return temp;
+    }
+
+    public void draw(BoardController controller, int xPos, int yPos) {
+        for (int y = 0; y < this.model.length; y++) {
+            for (int x = 0; x < this.model[y].length; x++) {
+                int[] rgba = this.model[y][x];
+                int[] rgb = new int[]{rgba[0], rgba[1], rgba[2]};
+
+                if(rgba[3] != 1) {
+                    int[] color;
+                    try {
+                        color = controller.getColorAt(x + xPos, y + yPos);
+                    } catch(ArrayIndexOutOfBoundsException e) {
+                        color = rgb;
+                    }
+                    controller.setColor(x + xPos, y + yPos, color);
+                } else {
+                    controller.setColor(x + xPos, y + yPos, rgb);
+                }
+            }
+        }
+    }
+}
