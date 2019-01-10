01f0630e2b6d30213259d5f5a9317339f9d6f0ec
lxender <hammes_alexander@yahoo.com>
2019-01-10 18:48:49 +0100

    Kommentare hinzugefügt

--- src/app/ledprojekt/AnimationManager.java
+++ src/app/ledprojekt/AnimationManager.java
@@ -11,10 +11,14 @@ public class AnimationManager {
     private HashMap<String, Boolean> animationPlayStates = new HashMap<>();
     private ArrayList<Model> currentlyPlayingAnimation = new ArrayList<>();
 
-    public static String findAnimation(HashMap<String, Boolean> animationPlayStates) {
-        if (animationPlayStates.containsValue(true)) {
-            for(String k : animationPlayStates.keySet()) {
-                if(animationPlayStates.get(k.toLowerCase())) {
+    /**
+     * Findet eine derzeit abspielende Animation im Manager.
+     * @return Den Namen der abspielenden Animation oder null wenn keine gefunden wurde.
+     */
+    public String findAnimation() {
+        if (this.animationPlayStates.containsValue(true)) {
+            for(String k : this.animationPlayStates.keySet()) {
+                if(this.animationPlayStates.get(k.toLowerCase())) {
                     return k;
                 }
             }
@@ -22,7 +26,15 @@ public class AnimationManager {
         return null;
     }
 
-    public static Model getFrame(String name, ArrayList<Model> currentlyPlayingAnimation, AnimationWrapper anim, HashMap<String, Boolean> animationPlayStates) {
+    /**
+     * Methode um ein Bild der abspielenden Animation zu bekommen.
+     * @param name Name der Animation
+     * @param currentlyPlayingAnimation Referenz zum Objekt, das die gerade spielende Animation beinhaltet
+     * @param anim die Animation, die abgespielt werden soll
+     * @param animationPlayStates Referenz zum Objekt, das alle Spielzustände beinhaltet
+     * @return Das nächste Bild der Animation
+     */
+    public Model getFrame(String name, ArrayList<Model> currentlyPlayingAnimation, AnimationWrapper anim, HashMap<String, Boolean> animationPlayStates) {
         if (currentlyPlayingAnimation.isEmpty()) {
             currentlyPlayingAnimation.addAll(anim.list);
         }
@@ -37,24 +49,40 @@ public class AnimationManager {
         return frame;
     }
 
+    /**
+     * Fügt dem animations-Objekt die übergebene Animation hinzu.
+     * @param name Name der Animation
+     * @param durationInMillis Dauer der Animation
+     * @param models Bilder der Animation als Modelle
+     */
     public void addAnimation(String name, int durationInMillis, Model... models) {
-        //System.out.println("Name: " + name + ", array: " + Arrays.toString(models));
         ArrayList<Model> list = new ArrayList<>(Arrays.asList(models));
         AnimationWrapper wrapper = new AnimationWrapper(durationInMillis, list);
         this.animations.put(name.toLowerCase(), wrapper);
     }
+
+    /**
+     * Setzt den Zustand der namentlich übergebenen Animation (in lowercase).
+     * @param name Name der Animation
+     * @param value Wert, auf den der Zustand gesetzt werden soll
+     */
     public void setAnimationPlayState(String name, boolean value) {
         this.animationPlayStates.put(name.toLowerCase(), value);
         this.timer = System.currentTimeMillis();
     }
+
+    /**
+     * Überprüft, ob eine Animation abgespielt werden soll und gibt das Bild der Animation (mit korrekten Timing) zurück.
+     * @return Bild der abspielenden Animation oder null wenn keine Animation abgespielt wird
+     */
     public Model playAnimation() {
         if (!this.animationPlayStates.isEmpty()) {
-            String playingAnimationName = AnimationManager.findAnimation(this.animationPlayStates);
+            String playingAnimationName = this.findAnimation();
 
             if (playingAnimationName != null) {
                 AnimationWrapper anim = this.animations.get(playingAnimationName);
                 if (System.currentTimeMillis() - this.timer > anim.msPerFrame) {
-                    Model frame = AnimationManager.getFrame(playingAnimationName, this.currentlyPlayingAnimation, anim, this.animationPlayStates);
+                    Model frame = this.getFrame(playingAnimationName, this.currentlyPlayingAnimation, anim, this.animationPlayStates);
                     this.timer += anim.msPerFrame;
 
                     return frame;
